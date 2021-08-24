69ffbc37cbf7e40d42f3e8ae6e9fe4b04af1552b
HectorLuis Herrera Castro <hherrera@estudiantec.cr>
2021-08-23 23:39:47 -0600

    funcion buscarVideos

--- App/VideoByArtistID/VideoByArtistID.js
+++ App/VideoByArtistID/VideoByArtistID.js
@@ -1,15 +1,61 @@
 //referencia
-const aplicacion = document.querySelector('.container')
+//const aplicacion = document.querySelector('.container')
+
+function buscar(idVideo)
+{   
+    var idVideo=document.getElementById("idVideo").value;
+    var detalles="";
+
+    if(idVideo==""){
+       
+        detalles ="<tr>" +
+        "<td  > Sin informacion </td>"+
+        "</tr>";
+        document.getElementById("informacion").innerHTML = detalles;
+        
+    }else  {
+       
+        if(window.XMLHttpRequest){
+            xmlhttp=new XMLHttpRequest();                   
+        } else{
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");        
+        } 
+            xmlhttp.onreadystatechange=function() {
+            if(this.readyState ==4 && this.status ==200){
+            var data= JSON.parse(this.responseText);
+                data.Search.forEach(video => {
+                    detalles += "<tr>"+
+                    
+                               "<td><a href='"+ movie.imdbID+"'>Link al video</td>"+
+                               "<td>"+ video.strTrack+"</td>"+
+                               //"<td>"+ video.strTrackThumb+"</td>"+
+                               "<td><img src="+ video.strTrackThumb+"></td>"+
+                               "</tr>";
+                               
+                              
+                                                    
+                });
+              
+                document.getElementById("informacion").innerHTML=detalles;
+               
+                }
+                
+            };           
+            xmlhttp.open("POST","https://theaudiodb.com/api/v1/json/1/mvid.php?i=" + idVideo, true);
+       
+            xmlhttp.send();
+        }
+}
 
 //var idVideo = document.getElementById("idVideo").value
 
 //peticion
-//const url = 'https://theaudiodb.com/api/v1/json/1/mvid.php?' + idVideo
+//const url = 'https://theaudiodb.com/api/v1/json/1/mvid.php?i=' + idVideo
 
 //peticion
-const url = 'https://theaudiodb.com/api/v1/json/1/mvid.php?i=112024'
+//const url = 'https://theaudiodb.com/api/v1/json/1/mvid.php?i=112024'
 
-fetch(url)
+/*fetch(url)
 //a estos .then se les llama promesas
 .then(respuesta => respuesta.json())//se captura todo el objeto json para poder recorrerlo
 .then(datos => {
@@ -26,4 +72,4 @@ fetch(url)
     //console.log(data.album)
 })
 //si alguna de las promesas falla capturamos el error
-.catch(err => console.log(err))
+.catch(err => console.log(err))*/
