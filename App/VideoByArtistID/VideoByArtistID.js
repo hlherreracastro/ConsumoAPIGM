5cbbd40dd5c0a7feafcdc92e94270eef36f78a10
HectorLuis Herrera Castro <hherrera@estudiantec.cr>
2021-08-25 14:28:46 -0600

    funcion buscarVideos

--- App/VideoByArtistID/VideoByArtistID.js
+++ App/VideoByArtistID/VideoByArtistID.js
@@ -1,34 +1,46 @@
-//referencia
-//const aplicacion = document.querySelector('.container')
+const d = document,
+$form = d.getElementById("song-search"),
+$loader = d.querySelector(".loader"),
+$error = d.querySelector(".error"),
+$main = d.querySelector("main"),
+$artist = d.querySelector(".artist");
 
-//var idVideo = document.getElementById("idVideo").value
 
-//peticion
-//const url = 'https://theaudiodb.com/api/v1/json/1/mvid.php?i=' + idVideo
+$form.addEventListener("submit", async e =>{
+e.preventDefault();
 
-//peticion
-//const url = 'https://theaudiodb.com/api/v1/json/1/mvid.php?i=112024'
+try{
+    $loader.style.display = "block";
 
-/*fetch(url)
-//a estos .then se les llama promesas
-.then(respuesta => respuesta.json())//se captura todo el objeto json para poder recorrerlo
-.then(datos => {
-    //datos.album da acceso al array que contiene los elementos o el contenido del archivo json
-    datos.mvids.forEach(video => { //tituloAlbum es la variable que recorrera cada uno de los elementos del array
-        console.log(video.strMusicVid, video.strTrack, video.strTrackThumb)//se captura el strAlbum que es un dato del array(en este caso el titulo) del json
-        //estas lineas muestran los datos al usuario en pantalla
-        const p = document.createElement('p')
-        p.innerHTML = "<div class='row gx-4 gx-lg-5 align-items-center my-5'>"+
-        "<div class='col-lg-5'>"+"<img class='img-fluid rounded mb-4 mb-lg-0' src=" + video.strTrackThumb +">"+"</div>"+
-        "<div class='col-lg-7'>"+
-            "<h1 class='font-weight-light'>" +video.strTrack+ "</h1>"+
-            "<a class='btn btn-primary' href="+ video.strMusicVid +">Go to Video!</a>"+
-        "</div>"+
-    "</div>"
-    
-        aplicacion.appendChild(p)
-    });
-    //console.log(data.album)
-})
-//si alguna de las promesas falla capturamos el error
-.catch(err => console.log(err))*/
+    let artist = e.target.artist.value.toLowerCase(), 
+    $artistTemplate = "", 
+    artistAPI=`https://theaudiodb.com/api/v1/json/1/mvid.php?i=${artist}`, 
+    artistFetch = fetch(artistAPI),
+    [artistRes] = await Promise.all([artistFetch]),
+    artistData = await artistRes.json();
+
+    //console.log(artistRes);
+    console.log(artistData);
+
+    if(artistData.mvids === null){
+        $artistTemplate = `<h2>No existe este ID Artist<mark>${artist}</mark></h2`
+    }
+    else{
+        let artist = artistData.mvids[7];
+        $artistTemplate = `
+        <h2>${artist.strTrack}</h2>
+        <img src="${artist.strTrackThumb}" alt="${artist.strTrack}">
+        <a href="${artist.strMusicVid}" target="_blank">Link al Video</a>
+        `;
+    }
+
+    $loader.style.display = "none";
+    $artist.innerHTML = $artistTemplate;
+
+}catch(err){
+    console.log(err);
+    let message = err.statusText || "Ocurrio un error";
+    $error.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
+    $loader.style.display = "none";
+}
+});
